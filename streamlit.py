12679a3159f9ea6860d1695295b35c0172da789f
reenusivadarshini <78981323+reenusivadarshini@users.noreply.github.com>
2022-09-24 14:50:06 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_chat import message as st_message
 from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
 from PIL import Image
-image = Image.open('1.jpg')
+image = Image.open('1.png')
 
 st.sidebar.image(image)
 @st.experimental_singleton
